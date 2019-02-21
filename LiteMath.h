edf39328d7fe1392b25803f23412f62b514d70b5
Nikita Bobko <nikitabobko+git@gmail.com>
2019-02-21 10:56:45 +0300

    Fix constructor bug in LiteMath.h

--- LiteMath.h
+++ LiteMath.h
@@ -46,7 +46,7 @@ namespace LiteMath {
 
         int3(int a, int b, int c) : x(a), y(b), z(c) {}
 
-        int3(const int *ptr) : x(ptr[0]), y(ptr[1]), z(ptr[0]) {}
+        int3(const int *ptr) : x(ptr[0]), y(ptr[1]), z(ptr[2]) {}
 
         int x, y, z;
     };
